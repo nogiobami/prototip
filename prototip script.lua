2d21a387d6562480e3b0d5d12fcbb5c1e16aa98e
nogiobami <kuznem30@gmail.com>
2025-08-20 16:43:46 +0300

    prototip script.lua

--- prototip script.lua
+++ prototip script.lua
@@ -1,3 +1,4 @@
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
 local Window = Library.CreateLib("Name", "RJTheme3")
@@ -53,7 +54,7 @@ while wait(0.5) do
 end
 end)
 
-Section:NewToggle("супер прыжок", "ToggleInfo", function(state)
+Section:NewToggle("superjump", "ToggleInfo", function(state)
     if state then
        game.player.localplayer.character.humanoid.jumppower = 500 
     else
@@ -61,52 +62,19 @@ Section:NewToggle("супер прыжок", "ToggleInfo", function(state)
     end
 end)
 
-Section:NewButton("ButtonText", "ButtonInfo", function()
-local uis = game:GetService("UserInputService")
-local rs = game:GetService("RunService")
-
-local myPlayer = game.Players.LocalPlayer
-local myChar = myPlayer.Character
-local myHRP = myChar:WaitForChild("HumanoidRootPart")
-local camera = game.Workspace.CurrentCamera
-
-local flying = false
-local speed = 0.5
-
-
-local bp = Instance.new("BodyPosition", myHRP)
-bp.MaxForce = Vector3.new()
-bp.D = 10
-bp.P = 10000
-
-local bg = Instance.new("BodyGyro", myHRP)
-bg.MaxTorque = Vector3.new()
-bg.D = 10
-
-function fly()
-  flying = true
-  bp.MaxForce = Vector3.new(400000,400000,400000)
-  bg.MaxTorque = Vector3.new(400000,400000,400000)
-  while flying do
-    rs.RenderStepped:wait()
-    bp.Position = myHRP.Position +((myHRP.Position - camera.CFrame.p).unit * speed)
-    bg.CFrame = CFrame.new(camera.CFrame.p, myHRP.Position)
-  end
-end
-
-function endFlying()
-  bp.MaxForce = Vector3.new()
-  bg.MaxTorque = Vector3.new()
-  flying = false
-end
-
-uis.InputBegan:connect(function(input)
-  if input.KeyCode == Enum.KeyCode.space then
-    if not flying then
-      fly()
-    else
-      endFlying()
-    end
-  end
+Section:NewToggle("teleport", "ToggleInfo", function(state)
+    if state then
+        mouse = game.Players.LocalPlayer:GetMouse()
+tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "Click Teleport"
+tool.Activated:connect(function()
+local pos = mouse.Hit+Vector3.new(0,2.5,0)
+pos = CFrame.new(pos.X,pos.Y,pos.Z)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
 end)
+tool.Parent = game.Players.LocalPlayer.Backpack
+    else
+        print("hi")
+    end
 end)
